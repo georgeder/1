499cc9e9388c83684adf8c1e52dafc00945dd2c5
georgeder <georg.christoph.eder@gmail.com>
2020-03-27 13:00:45 +0100

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,6 +1,6 @@
 
-# H1
-This file will be used to complete the Coursera IBM Data Science Capstone Final Project
+#
+This file will be used to complete the Coursera IBM Data Science Capstone Final Project 
 
 import pandas as pd
 import numpy as np
